905bb690a779535837a30b53c5a955f6de75185f
Ibrahim <buamod@gmail.com>
2016-03-02 22:23:20 -0800

    First commit creating the enviroment 2

---
+++ src/Testing/FirstTestingClass.java
@@ -0,0 +1,14 @@
+package Testing;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTestingClass {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new FirefoxDriver();
+		
+
+	}
+
+}
